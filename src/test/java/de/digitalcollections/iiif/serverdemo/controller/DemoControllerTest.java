d9f15399bb4e642f6b0ef12dcf414880ae1bb33f
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2017-01-25 17:05:05 +0100

    Added tests.

--- src/test/java/de/digitalcollections/iiif/serverdemo/controller/DemoControllerTest.java
+++ src/test/java/de/digitalcollections/iiif/serverdemo/controller/DemoControllerTest.java
@@ -1,6 +1,8 @@
 package de.digitalcollections.iiif.serverdemo.controller;
 
+import static org.hamcrest.CoreMatchers.is;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -11,7 +13,9 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -28,9 +32,77 @@ public class DemoControllerTest {
 
   @Test
   public void getHello() throws Exception {
-    ResultActions perform = mvc.perform(MockMvcRequestBuilders.get("/1234/view_image.html").
-            accept(MediaType.TEXT_HTML));
+    ResultActions perform = mvc.perform(MockMvcRequestBuilders.get("/1234/view_image.html").accept(MediaType.TEXT_HTML));
     perform.andExpect(MockMvcResultMatchers.status().isOk());
+  }
+
+  @Test
+  public void shouldGetHomepage() throws Exception {
+    mvc.perform(get("/"))
+        .andExpect(model().attribute("active", is("home")));
+  }
+
+  @Ignore
+  @Test
+  public void shouldGetAbout() throws Exception {
+    mvc.perform(get("/about"))
+        .andExpect(model().attribute("active", is("about")));
+  }
+
+  @Test
+  public void shouldGetImageApiUrlDemo() throws Exception {
+    mvc.perform(get("/image-api-url.html"))
+        .andExpect(model().attribute("active", is("demos")));
+  }
+
+  @Test
+  public void shouldGetPreviewPage() throws Exception {
+    mvc.perform(get("/12345/view_image.html"))
+        .andExpect(model().attribute("active", is("demos")))
+        .andExpect(model().attribute("infoUrl", is("/image/v2/12345/info.json")));
+  }
+
+  @Test
+  public void shouldGetIIPMooViewerPage() throws Exception {
+    mvc.perform(get("/12345/view_iipmoo.html"))
+        .andExpect(model().attribute("active", is("demos")))
+        .andExpect(model().attribute("serverUrl", is("/iiif/image/2.0.0/")))
+        .andExpect(model().attribute("image", is("12345")));
+  }
+
+  @Test
+  public void shouldGetLeafletImageViewerPage() throws Exception {
+    mvc.perform(get("/12345/view_leaflet-image.html"))
+        .andExpect(model().attribute("active", is("demos")))
+        .andExpect(model().attribute("infoUrl", is("/image/v2/12345/info.json")));
+  }
+
+  @Test
+  public void shouldGetLeafletManifestViewerPage() throws Exception {
+    mvc.perform(get("/12345/view_leaflet-presentation.html"))
+        .andExpect(model().attribute("active", is("demos")))
+        .andExpect(model().attribute("manifestId", is("/presentation/v2/12345/manifest")));
+  }
+
+  @Test
+  public void shouldGetMiradorPage() throws Exception {
+    mvc.perform(get("/12345/view_presentation.html"))
+        .andExpect(model().attribute("active", is("demos")))
+        .andExpect(model().attribute("manifestId", is("/presentation/v2/12345/manifest")));
+  }
+
+  @Test
+  public void shouldUniversalViewerPage() throws Exception {
+    mvc.perform(get("/12345/view_universal.html"))
+        .andExpect(model().attribute("active", is("demos")))
+        .andExpect(model().attribute("manifestId", is("/presentation/v2/12345/manifest")));
+  }
+
+  @Test
+  public void shouldDivaViewerPage() throws Exception {
+    mvc.perform(get("/12345/view_diva.html"))
+        .andExpect(model().attribute("active", is("demos")))
+        .andExpect(model().attribute("manifestId", is("/presentation/v2/12345/manifest")));
   }
 
 }
