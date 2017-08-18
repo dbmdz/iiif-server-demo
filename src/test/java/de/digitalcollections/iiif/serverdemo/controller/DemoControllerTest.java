47a17e9106794747fa25a77a6e29d385c3f873ce
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2017-08-18 14:16:13 +0200

    Refactor testcase

--- src/test/java/de/digitalcollections/iiif/serverdemo/controller/DemoControllerTest.java
+++ src/test/java/de/digitalcollections/iiif/serverdemo/controller/DemoControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 @SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @WebAppConfiguration
 public class DemoControllerTest {
 
