ff64351fa86496aad7d4eb0097ecbd1fc5c5f8a3
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2018-06-04 17:20:17 +0200

    Disable iframe security

--- src/main/java/de/digitalcollections/iiif/demo/config/SpringConfigSecurityDemo.java
+++ src/main/java/de/digitalcollections/iiif/demo/config/SpringConfigSecurityDemo.java
@@ -11,6 +11,8 @@ public class SpringConfigSecurityDemo extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
+            .headers().frameOptions().disable() // to make universalviewer work
+            .and()
             .authorizeRequests()
             .requestMatchers(EndpointRequest.to("info", "health")).permitAll()
             .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
