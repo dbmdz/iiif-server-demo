08fa3f2e94352d09d8314c36e6baff14ae41493c
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2019-01-08 13:09:17 +0100

    Upgrade libraries

--- src/main/java/de/digitalcollections/iiif/demo/config/SpringConfigDemo.java
+++ src/main/java/de/digitalcollections/iiif/demo/config/SpringConfigDemo.java
@@ -1,12 +1,13 @@
 package de.digitalcollections.iiif.demo.config;
 
+import de.digitalcollections.iiif.hymir.config.CustomResponseHeaders;
 import de.digitalcollections.iiif.hymir.config.SpringConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(SpringConfig.class)
+@Import({CustomResponseHeaders.class, SpringConfig.class})
 @ComponentScan(basePackages = {"de.digitalcollections.iiif.hymir.image", "de.digitalcollections.iiif.hymir.presentation"})
 public class SpringConfigDemo {
 
