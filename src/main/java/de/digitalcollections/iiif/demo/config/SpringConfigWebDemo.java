af46a77359e0510da652e99a1d3f815f9f6a1eb4
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2020-07-15 11:25:09 +0200

    Refactor webjar version to configuration properties

--- src/main/java/de/digitalcollections/iiif/demo/config/SpringConfigWebDemo.java
+++ src/main/java/de/digitalcollections/iiif/demo/config/SpringConfigWebDemo.java
@@ -2,18 +2,24 @@ package de.digitalcollections.iiif.demo.config;
 
 import de.digitalcollections.iiif.demo.interceptor.ServerUrlInterceptor;
 import de.digitalcollections.iiif.hymir.config.SpringConfigWeb;
+import de.digitalcollections.iiif.hymir.config.WebjarProperties;
 import de.digitalcollections.iiif.hymir.frontend.ExceptionAdvice;
 import de.digitalcollections.iiif.hymir.frontend.ExtendedViewController;
 import de.digitalcollections.iiif.hymir.frontend.GlobalControllerAdvice;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Import(SpringConfigWeb.class)
+@Import({
+  ExceptionAdvice.class,
+  ExtendedViewController.class,
+  GlobalControllerAdvice.class,
+  SpringConfigWeb.class,
+  WebjarProperties.class
+})
 public class SpringConfigWebDemo implements WebMvcConfigurer {
 
   @Autowired private ServerUrlInterceptor serverUrlInterceptor;
@@ -21,20 +27,5 @@ public class SpringConfigWebDemo implements WebMvcConfigurer {
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(serverUrlInterceptor);
-  }
-
-  @Bean
-  ExceptionAdvice exceptionAdvice() {
-    return new ExceptionAdvice();
-  }
-
-  @Bean
-  public ExtendedViewController extendedViewController() {
-    return new ExtendedViewController();
-  }
-
-  @Bean
-  GlobalControllerAdvice globalControllerAdvice() {
-    return new GlobalControllerAdvice();
   }
 }
